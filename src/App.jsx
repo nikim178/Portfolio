5ec705fe53a9308c78dcfbc05aa0991f6df680fc
Nikita Mundaye <nikitamundaye2002@gmail.com>
2024-02-21 10:40:32 +0530

    trying

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
-import GreenScreen from './components/GreenScreen.jsx';
+
 import Loader from './components/Loader.jsx';
 function App() {
     return(
         <div w-100>
             <Loader></Loader>
-            <GreenScreen></GreenScreen>
+           
         </div>
     )
 }
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,30 +1,69 @@
 import React, { useRef } from 'react';
-import { gsap, Circ } from 'gsap';
+import { gsap, Circ, Power3 } from 'gsap';
 import { useGSAP } from '@gsap/react';
 
 function Loader() {
   const reveal = useRef();
-  useGSAP(()=>{
-      gsap.to(
-        reveal.current.querySelectorAll('.animate'),{ 
-        y: "-100%", 
-        duration: 1, 
-        delay: 1, 
+  const green = useRef();
+  const tl = gsap.timeline();
+  useGSAP(() => {
+    tl.from(
+      reveal.current.querySelectorAll('span'),
+      {
+        x: 100,
+        delay: 0.5,
+        stagger: 0.2,
+        duration: 1,
+        ease: Power3.easeInOut
+      }
+    ).to(
+      reveal.current.querySelectorAll('.animate'), {
+        y: "-100%",
+        duration: 0.5,
+        delay: 0.5,
         ease: Circ.easeInOut,
       }
-      )
+    ).to(
+      reveal.current, {
+        y: "-100%",
+        height: 0,
+        duration: 1,
+        ease: Circ.easeInOut
+      },
+    ).to(
+      green.current, {
+        height: '100%',
+        top: 0,
+        duration: 1,
+        ease: Circ.easeInOut
+      },
+      ">-1"
+    ).to(
+      green.current,{
+        height: '0%',
+        duration: 1,
+        ease: Circ.easeInOut
+      }
+    )
   });
   
+  
   return (
-    <div className='w-100% h-screen bg-black' ref={reveal}>
-      <div className='block truncate text-white absolute top-8 left-1/2 transform -translate-x-1/2 font-sans font-thin text-sm text-center'>
-        <h5 className='animate'>Designing Portfolio <br/> &copy; 2024</h5>
-      </div>
-      <div className='w-24'>
-      <div className='block truncate text-7xl text-white font-sans font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'> 
-      <h1 className='animate'><span>Nikita </span><span className='italic font-display font-normal text-indigo-400'>Mundaye </span><span>is</span> <span>a</span></h1>
-      </div>
-      </div>
+    <div>
+      <section>
+        <div className='w-100% h-screen bg-black truncate' ref={reveal}>
+          <div className='block truncate text-white absolute top-8 left-1/2 transform -translate-x-1/2 font-sans font-thin text-sm text-center'>
+            <h5 className='animate'>Designing Portfolio <br/> &copy; 2024</h5>
+          </div>
+          <div className='block truncate text-7xl text-white font-sans font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'> 
+            <h1 className='animate h-24'><span className='inline-block'>Nikita</span> <span className='inline-block italic font-display font-normal text-green'>Mundaye </span> <span className='inline-block'>is</span> <span className='inline-block'>a</span></h1>
+          </div>
+          <div ref={green} className='absolute top-full w-100% h-0 bg-green'>
+
+          </div>
+        </div> 
+      </section>
+      
     </div>
   )
 }
